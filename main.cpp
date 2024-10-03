8f5c2455d567275b6d43eb18350a55622435e7b3
FlyingDevilMan <flyingdevilman@gmail.com>
2024-10-03 15:10:35 +0300

    antras kartas

--- main.cpp
+++ main.cpp
@@ -24,12 +24,12 @@ void PalygintiKursus(const string& valiuta1, const string& valiuta2, double kurs
 
 // Funkcija valiutai pirkti
 double PirktiValiuta(double suma, double kursas) {
-    return suma / kursas;
+    return suma * kursas;
 }
 
 // Funkcija valiutai parduoti
 double ParduotiValiuta(double suma, double kursas) {
-    return suma * kursas;
+    return suma / kursas;
 }
 
 int main() {
@@ -39,96 +39,104 @@ int main() {
     string nuoValiutos, ikiValiutos;
     double suma, rezultatas;
 
-    cout << "Pasirinkite veiksma:" << endl;
-    cout << "1. Palyginti valiutos kursus" << endl;
-    cout << "2. Pirkti valiuta" << endl;
-    cout << "3. Parduoti valiuta" << endl;
-    cin >> pasirinkimas;
+    // Ciklas, kuris leis programa kartoti tol, kol vartotojas nuspręs išeiti
+    while (true) {
+        cout << "Pasirinkite veiksma:" << endl;
+        cout << "1. Palyginti valiutos kursus" << endl;
+        cout << "2. Pirkti valiuta" << endl;
+        cout << "3. Parduoti valiuta" << endl;
+        cout << "4. Iseiti" << endl;
+        cin >> pasirinkimas;
 
-    if (pasirinkimas == 1) {
-        cout << "Iveskite pirmaja valiuta (EUR, USD, GBP, INR): ";
-        cin >> nuoValiutos;
-        cout << "Iveskite antraja valiuta (EUR, USD, GBP, INR): ";
-        cin >> ikiValiutos;
+        // Jei pasirinkimas yra 4 - išeiti iš programos
+        if (pasirinkimas == 4) {
+            cout << "Programa baigia darba." << endl;
+            break; // nutraukia while ciklą ir baigia programą
+        }
 
-        // Patikriname, ar įvestos valiutos skiriasi
-        if (nuoValiutos != ikiValiutos) {
-            // Kursų palyginimo logika
-            if (nuoValiutos == "EUR") {
-                if (ikiValiutos == "GBP") {
-                    PalygintiKursus(nuoValiutos, ikiValiutos, EUR_Total / GBP_Total, GBP_Total / EUR_Total);
-                } else if (ikiValiutos == "USD") {
-                    PalygintiKursus(nuoValiutos, ikiValiutos, EUR_Total / USD_Total, USD_Total / EUR_Total);
-                } else if (ikiValiutos == "INR") {
-                    PalygintiKursus(nuoValiutos, ikiValiutos, EUR_Total / INR_Total, INR_Total / EUR_Total);
+        if (pasirinkimas == 1) {
+            cout << "Iveskite pirmaja valiuta (EUR, USD, GBP, INR): ";
+            cin >> nuoValiutos;
+            cout << "Iveskite antraja valiuta (EUR, USD, GBP, INR): ";
+            cin >> ikiValiutos;
+
+            // Patikriname, ar įvestos valiutos skiriasi
+            if (nuoValiutos != ikiValiutos) {
+                if (nuoValiutos == "EUR") {
+                    if (ikiValiutos == "GBP") {
+                        PalygintiKursus(nuoValiutos, ikiValiutos, EUR_Total / GBP_Total, GBP_Total / EUR_Total);
+                    } else if (ikiValiutos == "USD") {
+                        PalygintiKursus(nuoValiutos, ikiValiutos, EUR_Total / USD_Total, USD_Total / EUR_Total);
+                    } else if (ikiValiutos == "INR") {
+                        PalygintiKursus(nuoValiutos, ikiValiutos, EUR_Total / INR_Total, INR_Total / EUR_Total);
+                    }
+                } else if (nuoValiutos == "GBP") {
+                    if (ikiValiutos == "USD") {
+                        PalygintiKursus(nuoValiutos, ikiValiutos, GBP_Total / USD_Total, USD_Total / GBP_Total);
+                    } else if (ikiValiutos == "INR") {
+                        PalygintiKursus(nuoValiutos, ikiValiutos, GBP_Total / INR_Total, INR_Total / GBP_Total);
+                    } else if (ikiValiutos == "EUR") {
+                        PalygintiKursus(nuoValiutos, ikiValiutos, GBP_Total / EUR_Total, EUR_Total / GBP_Total);
+                    }
+                } else if (nuoValiutos == "USD") {
+                    if (ikiValiutos == "GBP") {
+                        PalygintiKursus(nuoValiutos, ikiValiutos, USD_Total / GBP_Total, GBP_Total / USD_Total);
+                    } else if (ikiValiutos == "INR") {
+                        PalygintiKursus(nuoValiutos, ikiValiutos, USD_Total / INR_Total, INR_Total / USD_Total);
+                    } else if (ikiValiutos == "EUR") {
+                        PalygintiKursus(nuoValiutos, ikiValiutos, USD_Total / EUR_Total, EUR_Total / USD_Total);
+                    }
+                } else if (nuoValiutos == "INR") {
+                    if (ikiValiutos == "GBP") {
+                        PalygintiKursus(nuoValiutos, ikiValiutos, INR_Total / GBP_Total, GBP_Total / INR_Total);
+                    } else if (ikiValiutos == "USD") {
+                        PalygintiKursus(nuoValiutos, ikiValiutos, INR_Total / USD_Total, USD_Total / INR_Total);
+                    } else if (ikiValiutos == "EUR") {
+                        PalygintiKursus(nuoValiutos, ikiValiutos, INR_Total / EUR_Total, EUR_Total / INR_Total);
+                    }
                 }
-            } else if (nuoValiutos == "GBP") {
-                if (ikiValiutos == "USD") {
-                    PalygintiKursus(nuoValiutos, ikiValiutos, GBP_Total / USD_Total, USD_Total / GBP_Total);
-                } else if (ikiValiutos == "INR") {
-                    PalygintiKursus(nuoValiutos, ikiValiutos, GBP_Total / INR_Total, INR_Total / GBP_Total);
-                } else if (ikiValiutos == "EUR") {
-                    PalygintiKursus(nuoValiutos, ikiValiutos, GBP_Total / EUR_Total, EUR_Total / GBP_Total);
-                }
-            } else if (nuoValiutos == "USD") {
-                if (ikiValiutos == "GBP") {
-                    PalygintiKursus(nuoValiutos, ikiValiutos, USD_Total / GBP_Total, GBP_Total / USD_Total);
-                } else if (ikiValiutos == "INR") {
-                    PalygintiKursus(nuoValiutos, ikiValiutos, USD_Total / INR_Total, INR_Total / USD_Total);
-                } else if (ikiValiutos == "EUR") {
-                    PalygintiKursus(nuoValiutos, ikiValiutos, USD_Total / EUR_Total, EUR_Total / USD_Total);
-                }
-            } else if (nuoValiutos == "INR") {
-                if (ikiValiutos == "GBP") {
-                    PalygintiKursus(nuoValiutos, ikiValiutos, INR_Total / GBP_Total, GBP_Total / INR_Total);
-                } else if (ikiValiutos == "USD") {
-                    PalygintiKursus(nuoValiutos, ikiValiutos, INR_Total / USD_Total, USD_Total / INR_Total);
-                } else if (ikiValiutos == "EUR") {
-                    PalygintiKursus(nuoValiutos, ikiValiutos, INR_Total / EUR_Total, EUR_Total / INR_Total);
-                }
+            } else {
+                cout << "Sios dvi valiutos yra vienodos." << endl;
             }
+        } else if (pasirinkimas == 2) {
+            cout << "Iveskite valiuta, kuria norite pirkti (USD, GBP, INR): ";
+            cin >> ikiValiutos;
+            cout << "Iveskite suma EUR, kuria norite pirkti: ";
+            cin >> suma;
+
+            if (ikiValiutos == "GBP") {
+                rezultatas = PirktiValiuta(suma, GBP_Buy);
+            } else if (ikiValiutos == "USD") {
+                rezultatas = PirktiValiuta(suma, USD_Buy);
+            } else if (ikiValiutos == "INR") {
+                rezultatas = PirktiValiuta(suma, INR_Buy);
+            } else {
+                cout << "Netinkama valiuta." << endl;
+                continue; // grįžtame į pradžią, jeigu buvo klaida
+            }
+
+            cout << "Jus gausite " << fixed << setprecision(2) << rezultatas << " " << ikiValiutos << endl;
+        } else if (pasirinkimas == 3) {
+            cout << "Iveskite valiuta, kuria norite parduoti (USD, GBP, INR): ";
+            cin >> nuoValiutos;
+            cout << "Iveskite suma " << nuoValiutos << ", kuria norite parduoti: ";
+            cin >> suma;
+
+            if (nuoValiutos == "GBP") {
+                rezultatas = ParduotiValiuta(suma, GBP_Sell);
+            } else if (nuoValiutos == "USD") {
+                rezultatas = ParduotiValiuta(suma, USD_Sell);
+            } else if (nuoValiutos == "INR") {
+                rezultatas = ParduotiValiuta(suma, INR_Sell);
+            } else {
+                cout << "Netinkama valiuta." << endl;
+                continue; // grįžtame į pradžią, jeigu buvo klaida
+            }
+
+            cout << "Jus gausite " << fixed << setprecision(2) << rezultatas << " EUR" << endl;
         } else {
-            cout << "Sios dvi valiutos yra vienodos." << endl;
+            cout << "Netinkamas pasirinkimas." << endl;
         }
-    } else if (pasirinkimas == 2) {
-        cout << "Iveskite valiuta, kuria norite pirkti (USD, GBP, INR): ";
-        cin >> ikiValiutos;
-        cout << "Iveskite suma EUR, kuria norite pirkti: ";
-        cin >> suma;
-
-        if (ikiValiutos == "GBP") {
-            rezultatas = PirktiValiuta(suma, GBP_Buy);
-        } else if (ikiValiutos == "USD") {
-            rezultatas = PirktiValiuta(suma, USD_Buy);
-        } else if (ikiValiutos == "INR") {
-            rezultatas = PirktiValiuta(suma, INR_Buy);
-        } else {
-            cout << "Netinkama valiuta." << endl;
-            return 1;
-        }
-
-        cout << "Jus gausite " << fixed << setprecision(2) << rezultatas << " " << ikiValiutos << endl;
-    } else if (pasirinkimas == 3) {
-        cout << "Iveskite valiuta, kuria norite parduoti (USD, GBP, INR): ";
-        cin >> nuoValiutos;
-        cout << "Iveskite suma " << nuoValiutos << ", kuria norite parduoti: ";
-        cin >> suma;
-
-        if (nuoValiutos == "GBP") {
-            rezultatas = ParduotiValiuta(suma, GBP_Sell);
-        } else if (nuoValiutos == "USD") {
-            rezultatas = ParduotiValiuta(suma, USD_Sell);
-        } else if (nuoValiutos == "INR") {
-            rezultatas = ParduotiValiuta(suma, INR_Sell);
-        } else {
-            cout << "Netinkama valiuta." << endl;
-            return 1;
-        }
-
-        cout << "Jus gausite " << fixed << setprecision(2) << rezultatas << " EUR" << endl;
-    } else {
-        cout << "Netinkamas pasirinkimas." << endl;
-        return 1;
     }
 
     return 0;
